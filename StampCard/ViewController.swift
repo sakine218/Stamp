962161d35cf9e064537ba03985e7e787767fb8aa
Sakine Nishibayashi <snishibayashi218@gmail.com>
2020-07-30 15:29:04 +0900

    Initial Commit

---
+++ StampCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StampCard
+//
+//  Created by Sakine Nishibayashi on 2020/07/30.
+//  Copyright © 2020 sakine.nishibayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
